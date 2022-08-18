261e83f97f9b471832efe8e6e43088a35d9ef2d6
Mohammad Sawad T A <mohta@publicisgroupe.net>
2022-08-18 18:31:30 +0530

    initial commit

---
+++ concatination-test/src/main/java/com/demo/concat/Concatination.java
@@ -0,0 +1,10 @@
+package com.demo.concat;
+
+public class Concatination
+{
+	
+	public String twoNameConcat(String firstName,String lastName)
+	{
+		return firstName+" ".concat(lastName);
+	}
+}
---
+++ concatination-test/src/main/java/com/demo/concat/Main.java
@@ -0,0 +1,11 @@
+package com.demo.concat;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		Concatination concatination = new Concatination();
+		System.out.println("Hello "+concatination.twoNameConcat("Mohammad", "Sawad"));
+	}
+
+}
